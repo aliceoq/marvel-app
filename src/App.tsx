400c56e4a4a5f0830061130056bb8cdadfdccf82
aliceoq <aliceoqbrito@gmail.com>
2024-01-17 20:04:42 -0300

    feat: add to app the QueryClientProvider and Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { httpRequest } from "./services/api";
+import {
+  QueryClient,
+  QueryClientProvider,
+  type QueryKey,
+} from "@tanstack/react-query";
+import { AxiosRequestConfig, Method } from "axios";
+import Router from "./Router";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const defaultQueryFn = ({
+    queryKey,
+  }: {
+    queryKey: QueryKey | (Method | string | AxiosRequestConfig<any>)[];
+  }): void | Promise<any> => {
+    if (!queryKey || !queryKey.length) return;
+
+    return httpRequest(queryKey);
+  };
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        refetchOnMount: true,
+        queryFn: defaultQueryFn,
+      },
+    },
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <Router />
+      </QueryClientProvider>
+    </Provider>
+  );
 }
 
-export default App
+export default App;
