d90a9a3d2dd79156e361f39d18db0c9b96e880fd
aliceoq <aliceoqbrito@gmail.com>
2024-01-21 10:05:09 -0300

    feat(components): create spinner

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,14 @@
+import { Ellipsis } from "./styles";
+
+const Spinner = () => {
+  return (
+    <Ellipsis>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </Ellipsis>
+  );
+};
+
+export default Spinner;
